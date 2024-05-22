07219464d3b63b4fd40a912d04328d024c4d698c
R.Gayathre Ranga Sri <128204314+Grs-6@users.noreply.github.com>
2024-05-22 23:20:01 +0530

    Update Main.java

--- Bridge/Main.java
+++ Bridge/Main.java
@@ -3,10 +3,13 @@ public class Main {
         Renderer rasterRenderer = new RasterRenderer(800, 600, true, 24);
         Renderer vectorRenderer = new VectorRenderer(0.01, 1.0, "Cartesian");
 
-        Shape circle = new Circle(rasterRenderer, 50, 50, 40, "black", 3, "red");
+        Shape circle = new Circle(rasterRenderer, 50, 50, 20, "black", 3, "red");
         Shape rectangle = new Rectangle(vectorRenderer, 10, 10, 100, 50, "blue", 3, "green");
 
         circle.draw();
+        System.out.println("Circle Dimensions: " + circle.getDimensions());
+
         rectangle.draw();
+        System.out.println("Rectangle Dimensions: " + rectangle.getDimensions());
     }
 }
