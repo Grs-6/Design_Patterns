6e402260b162bb6972319af519f1664143aa1863
R.Gayathre Ranga Sri <128204314+Grs-6@users.noreply.github.com>
2024-05-22 23:21:47 +0530

    Delete Bridge directory

--- Bridge/Renderer.java
+++
@@ -1,5 +0,0 @@
-interface Renderer {
-    void renderCommonAttributes();
-    void renderCircle(int cx, int cy, int radius, String strokeColor, int strokeWidth, String fillColor);
-    void renderRectangle(int x, int y, int width, int height, String strokeColor, int strokeWidth, String fillColor);
-}
--- Bridge/Shape.java
+++
@@ -1,9 +0,0 @@
-abstract class Shape {
-    protected Renderer renderer;
-
-    public Shape(Renderer renderer) {
-        this.renderer = renderer;
-    }
-
-    abstract void draw();
-}
