d6e1cf17ea6a1dd3c4bb01e6f1acbd8edf8e2964
R.Gayathre Ranga Sri <128204314+Grs-6@users.noreply.github.com>
2024-06-04 07:43:21 +0530

    Delete Facade directory

--- Facade/BookCatalog.java
+++
@@ -1,9 +0,0 @@
-public class BookCatalog {
-    public void searchBook(String title) {
-        System.out.println("Searching for book: " + title);
-    }
-
-    public void checkBookAvailability(String title) {
-        System.out.println("Checking availability for book: " + title);
-    }
-}
